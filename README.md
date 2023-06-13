823e28ea048622910b2c68ebd45028ace494575e
davidrg <davidrg@davidrgfoss.com>
2023-06-13 02:30:02 +0200

    t1

--- README.md
+++
@@ -1,1 +0,0 @@
-# p1-docker-php
--- mkbd.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-mysql -u $user_bookmedik --password=$password_bookmedik -h $password_bookmedik $namebd_bookmedik < /var/www/html/schema.sql
-
-/usr/sbin/apache2ctl -D FOREGROUND
